9fbfd8978e93da431cfde9ef94faa357384362ff
Christina Ausley <christinamausley@gmail.com>
2020-08-11 13:15:56 -0700

    cleaned up code

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -23,7 +23,6 @@ const DataTable = () => {
                   key={name}
                   style={{ width }}
                   onClick={() => {
-                    // context.handleSort(name.toLowerCase());
                     context.handleSort(name);
                   }}
                 >
